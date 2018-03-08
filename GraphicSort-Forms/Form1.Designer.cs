b12446e51a7f0470ca1648b61710472e73e814e9
DerSteph <stephchan420@yahoo.de>
2018-03-08 18:22:58 +0100

    version 0.0.1
    code based on my old program graphicsort

---
+++ GraphicSort-Forms/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace GraphicSort_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BubbleSort = new System.Windows.Forms.Button();
+            this.InsertionSort = new System.Windows.Forms.Button();
+            this.SelectionSort = new System.Windows.Forms.Button();
+            this.StephSort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // BubbleSort
+            // 
+            this.BubbleSort.Location = new System.Drawing.Point(12, 618);
+            this.BubbleSort.Name = "BubbleSort";
+            this.BubbleSort.Size = new System.Drawing.Size(75, 23);
+            this.BubbleSort.TabIndex = 1;
+            this.BubbleSort.Text = "BubbleSort";
+            this.BubbleSort.UseVisualStyleBackColor = true;
+            this.BubbleSort.Click += new System.EventHandler(this.BubbleSort_Click);
+            // 
+            // InsertionSort
+            // 
+            this.InsertionSort.Location = new System.Drawing.Point(93, 618);
+            this.InsertionSort.Name = "InsertionSort";
+            this.InsertionSort.Size = new System.Drawing.Size(75, 23);
+            this.InsertionSort.TabIndex = 2;
+            this.InsertionSort.Text = "InsertionSort";
+            this.InsertionSort.UseVisualStyleBackColor = true;
+            this.InsertionSort.Click += new System.EventHandler(this.InsertionSort_Click);
+            // 
+            // SelectionSort
+            // 
+            this.SelectionSort.Location = new System.Drawing.Point(174, 618);
+            this.SelectionSort.Name = "SelectionSort";
+            this.SelectionSort.Size = new System.Drawing.Size(83, 23);
+            this.SelectionSort.TabIndex = 3;
+            this.SelectionSort.Text = "SelectionSort";
+            this.SelectionSort.UseVisualStyleBackColor = true;
+            this.SelectionSort.Click += new System.EventHandler(this.SelectionSort_Click);
+            // 
+            // StephSort
+            // 
+            this.StephSort.Location = new System.Drawing.Point(263, 618);
+            this.StephSort.Name = "StephSort";
+            this.StephSort.Size = new System.Drawing.Size(75, 23);
+            this.StephSort.TabIndex = 4;
+            this.StephSort.Text = "StephSort";
+            this.StephSort.UseVisualStyleBackColor = true;
+            this.StephSort.Click += new System.EventHandler(this.StephSort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(324, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "click me to generate a random array";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 651);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StephSort);
+            this.Controls.Add(this.SelectionSort);
+            this.Controls.Add(this.InsertionSort);
+            this.Controls.Add(this.BubbleSort);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BubbleSort;
+        private System.Windows.Forms.Button InsertionSort;
+        private System.Windows.Forms.Button SelectionSort;
+        private System.Windows.Forms.Button StephSort;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GraphicSort-Forms/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace GraphicSort_Forms
+{
+    public partial class Form1 : Form
+    {
+        // Globale Variablen und Objekte
+        public static Random rnd = new Random();
+        static Pen stift = new Pen(Color.Red, 8);
+        static Pen deleter = new Pen(Color.LightGray, 8);
+        static Graphics grafik;
+        static int[] a = new int[100];
+
+        public Form1()
+        {
+            InitializeComponent();
+            // beim Start muss er ein Random Bild laden
+            a = Enumerable.Range(0, 100).OrderBy(c => rnd.Next()).ToArray();
+            grafik = this.pictureBox1.CreateGraphics();
+        }
+        private void BubbleSort_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a.Length - 1 - i; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int h = a[j + 1];
+                        a[j + 1] = a[j];
+                        a[j] = h;
+                        ChangePosition(j, j+1);
+                    }
+                }
+            }
+            for(int i = 0; i < 100; i++)
+            {
+                Debug.Write(a[i]);
+            }
+        }
+
+        private void InsertionSort_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < a.Length; i++)
+            {
+                int k = i;
+                for (k = i; k > 0; k--)
+                {
+                    if (a[k] < a[k - 1])
+                    {
+                        int h = a[k];
+                        a[k] = a[k - 1];
+                        a[k - 1] = h;
+                        ChangePosition(k, k-1);
+                    }
+                }
+            }
+        }
+        private void ChangePosition(int x1, int x2)
+        {
+            grafik.DrawLine(deleter, 5 + x1 * 8, 600, 5 + x1 * 8, 600 - a[x2] * 6 - 6);
+            grafik.DrawLine(stift, 5 + x1 * 8, 600, 5 + x1 * 8, 600 - a[x1] * 6-6);
+            grafik.DrawLine(deleter, 5 + x2 * 8, 600, 5 + x2 * 8, 600 - a[x1] * 6 - 6);
+            grafik.DrawLine(stift, 5 + x2 * 8, 600, 5 + x2 * 8, 600 - a[x2] * 6-6);
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            a = Enumerable.Range(0, 100).OrderBy(c => rnd.Next()).ToArray();
+            for (int i = 0; i < 100; i++)
+            {
+                grafik.DrawLine(deleter, 5 + i * 8, 600, 5 + i * 8, 0);
+                grafik.DrawLine(stift, 5 + i * 8, 600, 5 + i * 8, 600 - a[i] * 6 - 6);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            pictureBox1_Click_1(sender, e);
+        }
+
+        private void SelectionSort_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i; j < a.Length; j++)
+                {
+                    if (a[j] < a[min])
+                    {
+                        min = j;
+                    }
+                }
+                int h = a[min];
+                a[min] = a[i];
+                a[i] = h;
+                ChangePosition(min, i);
+                Thread.Sleep(50);
+            }
+        }
+
+        private void StephSort_Click(object sender, EventArgs e)
+        {
+            bool solange = true;
+            while (solange == true)
+            {
+                solange = false;
+                for (int i = 0; i < a.Length - 1; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        solange = true;
+                        int h = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = h;
+                        Console.Beep(5000 + 50 * a[i], 20);
+                        ChangePosition(i, i + 1);
+                    }
+                }
+            }
+        }
+    }
+}
