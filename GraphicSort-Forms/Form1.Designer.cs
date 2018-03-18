2fd5124848d89ed8008fd92a4002e69d77110183
DerSteph <stephchan420@yahoo.de>
2018-03-18 22:09:34 +0100

    version 0.0.2
    one more algorithm
    design features

--- GraphicSort-Forms/Form1.Designer.cs
+++ GraphicSort-Forms/Form1.Designer.cs
@@ -29,11 +29,15 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.BubbleSort = new System.Windows.Forms.Button();
-            this.InsertionSort = new System.Windows.Forms.Button();
-            this.SelectionSort = new System.Windows.Forms.Button();
-            this.StephSort = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,47 +49,6 @@
             this.pictureBox1.Size = new System.Drawing.Size(800, 600);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
-            // 
-            // BubbleSort
-            // 
-            this.BubbleSort.Location = new System.Drawing.Point(12, 618);
-            this.BubbleSort.Name = "BubbleSort";
-            this.BubbleSort.Size = new System.Drawing.Size(75, 23);
-            this.BubbleSort.TabIndex = 1;
-            this.BubbleSort.Text = "BubbleSort";
-            this.BubbleSort.UseVisualStyleBackColor = true;
-            this.BubbleSort.Click += new System.EventHandler(this.BubbleSort_Click);
-            // 
-            // InsertionSort
-            // 
-            this.InsertionSort.Location = new System.Drawing.Point(93, 618);
-            this.InsertionSort.Name = "InsertionSort";
-            this.InsertionSort.Size = new System.Drawing.Size(75, 23);
-            this.InsertionSort.TabIndex = 2;
-            this.InsertionSort.Text = "InsertionSort";
-            this.InsertionSort.UseVisualStyleBackColor = true;
-            this.InsertionSort.Click += new System.EventHandler(this.InsertionSort_Click);
-            // 
-            // SelectionSort
-            // 
-            this.SelectionSort.Location = new System.Drawing.Point(174, 618);
-            this.SelectionSort.Name = "SelectionSort";
-            this.SelectionSort.Size = new System.Drawing.Size(83, 23);
-            this.SelectionSort.TabIndex = 3;
-            this.SelectionSort.Text = "SelectionSort";
-            this.SelectionSort.UseVisualStyleBackColor = true;
-            this.SelectionSort.Click += new System.EventHandler(this.SelectionSort_Click);
-            // 
-            // StephSort
-            // 
-            this.StephSort.Location = new System.Drawing.Point(263, 618);
-            this.StephSort.Name = "StephSort";
-            this.StephSort.Size = new System.Drawing.Size(75, 23);
-            this.StephSort.TabIndex = 4;
-            this.StephSort.Text = "StephSort";
-            this.StephSort.UseVisualStyleBackColor = true;
-            this.StephSort.Click += new System.EventHandler(this.StephSort_Click);
             // 
             // label1
             // 
@@ -95,22 +58,108 @@
             this.label1.Size = new System.Drawing.Size(176, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "click me to generate a random array";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DisplayMember = "Text";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(90, 620);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.ValueMember = "ID";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(737, 618);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "start sorting";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 623);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "sort algorithm:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(656, 618);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "reset picture";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DisplayMember = "Text";
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(264, 620);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.ValueMember = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(217, 623);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "design:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(391, 623);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "color:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DisplayMember = "Text";
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(430, 620);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 13;
+            this.comboBox3.ValueMember = "ID";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(824, 651);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.StephSort);
-            this.Controls.Add(this.SelectionSort);
-            this.Controls.Add(this.InsertionSort);
-            this.Controls.Add(this.BubbleSort);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "GraphicSort";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -120,11 +169,15 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button BubbleSort;
-        private System.Windows.Forms.Button InsertionSort;
-        private System.Windows.Forms.Button SelectionSort;
-        private System.Windows.Forms.Button StephSort;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
 
--- GraphicSort-Forms/Form1.cs
+++ GraphicSort-Forms/Form1.cs
@@ -27,8 +27,33 @@ namespace GraphicSort_Forms
             // beim Start muss er ein Random Bild laden
             a = Enumerable.Range(0, 100).OrderBy(c => rnd.Next()).ToArray();
             grafik = this.pictureBox1.CreateGraphics();
+            Thread.Sleep(200);
+            for (int i = 0; i < 100; i++)
+            {
+                grafik.DrawLine(deleter, 5 + i * 8, 600, 5 + i * 8, 0);
+                grafik.DrawLine(stift, 5 + i * 8, 600, 5 + i * 8, 600 - a[i] * 6 - 6);
+            }
+            comboBox1.DataSource = new ComboItem[]
+            {
+                new ComboItem {ID = 1, Text = "BubbleSort"},
+                new ComboItem {ID = 2, Text = "StephSort"},
+                new ComboItem {ID = 3, Text = "InsertionSort"},
+                new ComboItem {ID = 4, Text = "MinSelectionSort"},
+                new ComboItem {ID = 5, Text = "MaxSelectionSort"}
+            };
+            comboBox2.DataSource = new ComboItem[]
+            {
+                new ComboItem {ID = 1, Text = "bar"},
+                new ComboItem {ID = 2, Text = "pixel"}
+            };
+            comboBox3.DataSource = new ComboItem[]
+            {
+                new ComboItem {ID = 1, Text = "blue"},
+                new ComboItem {ID = 2, Text = "red"},
+                new ComboItem {ID = 3, Text = "green"}
+            };
         }
-        private void BubbleSort_Click(object sender, EventArgs e)
+        private void BubbleSort()
         {
             for (int i = 0; i < a.Length; i++)
             {
@@ -39,17 +64,17 @@ namespace GraphicSort_Forms
                         int h = a[j + 1];
                         a[j + 1] = a[j];
                         a[j] = h;
-                        ChangePosition(j, j+1);
+                        ChangePosition(j, j + 1);
                     }
                 }
             }
-            for(int i = 0; i < 100; i++)
+            for (int i = 0; i < 100; i++)
             {
                 Debug.Write(a[i]);
             }
         }
 
-        private void InsertionSort_Click(object sender, EventArgs e)
+        private void InsertionSort()
         {
             for (int i = 1; i < a.Length; i++)
             {
@@ -61,41 +86,40 @@ namespace GraphicSort_Forms
                         int h = a[k];
                         a[k] = a[k - 1];
                         a[k - 1] = h;
-                        ChangePosition(k, k-1);
+                        ChangePosition(k, k - 1);
                     }
                 }
             }
         }
-        private void ChangePosition(int x1, int x2)
+        private void MaxSelectionSort()
         {
-            grafik.DrawLine(deleter, 5 + x1 * 8, 600, 5 + x1 * 8, 600 - a[x2] * 6 - 6);
-            grafik.DrawLine(stift, 5 + x1 * 8, 600, 5 + x1 * 8, 600 - a[x1] * 6-6);
-            grafik.DrawLine(deleter, 5 + x2 * 8, 600, 5 + x2 * 8, 600 - a[x1] * 6 - 6);
-            grafik.DrawLine(stift, 5 + x2 * 8, 600, 5 + x2 * 8, 600 - a[x2] * 6-6);
-        }
-
-        private void pictureBox1_Click_1(object sender, EventArgs e)
-        {
-            label1.Visible = false;
-            a = Enumerable.Range(0, 100).OrderBy(c => rnd.Next()).ToArray();
-            for (int i = 0; i < 100; i++)
+            for (int i = a.Length - 1; i >= 0; i--)
             {
-                grafik.DrawLine(deleter, 5 + i * 8, 600, 5 + i * 8, 0);
-                grafik.DrawLine(stift, 5 + i * 8, 600, 5 + i * 8, 600 - a[i] * 6 - 6);
+                int max = i;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (a[j] > a[max])
+                    {
+                        max = j;
+                    }
+                }
+                int h = a[max];
+                a[max] = a[i];
+                a[i] = h;
+                ChangePosition(i, max);
+                Thread.Sleep(50);
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                Debug.Write(a[i] + ", ");
             }
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-            pictureBox1_Click_1(sender, e);
-        }
-
-        private void SelectionSort_Click(object sender, EventArgs e)
+        private void MinSelectionSort()
         {
             for (int i = 0; i < a.Length - 1; i++)
             {
                 int min = i;
-                for (int j = i; j < a.Length; j++)
+                for (int j = i + 1; j < a.Length; j++)
                 {
                     if (a[j] < a[min])
                     {
@@ -110,7 +134,28 @@ namespace GraphicSort_Forms
             }
         }
 
-        private void StephSort_Click(object sender, EventArgs e)
+        private void SetColor()
+        {
+            int auswahl = (int)comboBox3.SelectedValue;
+            if (auswahl == 1)
+            {
+                stift.Color = Color.Blue;
+            }
+            else if (auswahl == 2)
+            {
+                stift.Color = Color.Red;
+            }
+            else if (auswahl == 3)
+            {
+                stift.Color = Color.Green;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void StephSort()
         {
             bool solange = true;
             while (solange == true)
@@ -130,5 +175,91 @@ namespace GraphicSort_Forms
                 }
             }
         }
+
+        private void ChangePosition(int x1, int x2)
+        {
+            int aussehen = (int)comboBox2.SelectedValue;
+
+            if (aussehen == 1)
+            {
+                grafik.DrawLine(deleter, 5 + x1 * 8, 600, 5 + x1 * 8, 600 - a[x2] * 6 - 6);
+                grafik.DrawLine(stift, 5 + x1 * 8, 600, 5 + x1 * 8, 600 - a[x1] * 6 - 6);
+                grafik.DrawLine(deleter, 5 + x2 * 8, 600, 5 + x2 * 8, 600 - a[x1] * 6 - 6);
+                grafik.DrawLine(stift, 5 + x2 * 8, 600, 5 + x2 * 8, 600 - a[x2] * 6 - 6);
+            }
+            else
+            {
+                grafik.DrawLine(deleter, 5 + x1 * 8, 600 - a[x2] * 6 - 8, 5 + x1 * 8, 600 - a[x2] * 6);
+                grafik.DrawLine(stift, 5 + x1 * 8, 600 - a[x1] * 6 - 8, 5 + x1 * 8, 600 - a[x1] * 6);
+                grafik.DrawLine(deleter, 5 + x2 * 8, 600 - a[x1] * 6 - 8, 5 + x2 * 8, 600 - a[x1] * 6);
+                grafik.DrawLine(stift, 5 + x2 * 8, 600 - a[x2] * 6 - 8, 5 + x2 * 8, 600 - a[x2] * 6);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int auswahl = (int)comboBox1.SelectedValue;
+            int aussehen = (int)comboBox2.SelectedValue;
+            SetColor();
+            if (auswahl == 1)
+            {
+                BubbleSort();
+            }
+            else if (auswahl == 2)
+            {
+                StephSort();
+            }
+            else if (auswahl == 3)
+            {
+                InsertionSort();
+            }
+            else if (auswahl == 4)
+            {
+                MinSelectionSort();
+            }
+            else if (auswahl == 5)
+            {
+                MaxSelectionSort();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            button1.Enabled = true;
+            a = Enumerable.Range(0, 100).OrderBy(c => rnd.Next()).ToArray();
+            int aussehen = (int)comboBox2.SelectedValue;
+            SetColor();
+            if (aussehen == 1)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    grafik.DrawLine(deleter, 5 + i * 8, 600, 5 + i * 8, 0);
+                    grafik.DrawLine(stift, 5 + i * 8, 600, 5 + i * 8, 600 - a[i] * 6 - 6);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    grafik.DrawLine(deleter, 5 + i * 8, 600, 5 + i * 8, 0);
+                    grafik.DrawLine(stift, 5 + i * 8, 600 - a[i] * 6 - 8, 5 + i * 8, 600 - a[i] * 6);
+                }
+            }
+        }
+    }
+    class ComboItem
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
     }
 }
